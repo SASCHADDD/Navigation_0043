8e343239934194a5cbdca6c6b2fa552b0de7e2a8
SASCHADDD <saschadanu23@gmail.com>
2026-03-13 22:03:31 +0700

    membuat tampilan dengan memanggil dari main layout dan memanggil file gambar di folder lokal

---
+++ navigation_0043/lib/auth/login.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      Title: 'Login',
+      showAppBar: false,
+      child: Container(
+        color: MainLayout.backgroundColor,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 24.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset(
+                      'assets/images/login.png',
+                      height: 200,
+                      fit: BoxFit.contain,
+                      errorBuilder: (context, error, StackTrace){
+                        return Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: Main.Layout.primaryColor.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          
+                        )
+                      },
+                    ),
+                    ).
+                  ],
+
+                ),,
+              ),
+              ),,
+      )
+}
+
